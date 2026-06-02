1d66aad17b904d918ba016e7c624000e30663aab
szaroket <30178095+szaroket@users.noreply.github.com>
2026-06-02 14:20:15 +0200

    feat: add simple endpoints to backend

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/health")
+async def healt_check():
+    return {"status": "healthy"}
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app=app, host="localhost", port=8000)
