ff1a791efeeba4f6c80087104173a4eac1b249b6
szaroket <30178095+szaroket@users.noreply.github.com>
2026-07-06 18:51:43 +0200

    feat: update version to 0.8.0

--- frontend/src/app/components/app-footer.tsx
+++ frontend/src/app/components/app-footer.tsx
@@ -1,7 +1,7 @@
 export function AppFooter() {
   return (
     <footer className="border-t border-slate-700 py-3 text-center text-xs text-slate-500">
-      v0.7.0
+      v0.8.0
     </footer>
   );
 }
